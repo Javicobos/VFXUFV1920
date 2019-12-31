edc572ae7350b0e917b61f4a991525fbf4500914
MiguelBS <miguelbsab@gmail.com>
2019-12-31 21:16:06 +0100

    Proyecto final

---
+++ VFX Final/Assets/Scripts/CheckerTransition.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckerTransition : MonoBehaviour
+{
+    Renderer rend;
+    float freq;
+    public Shader shader1;
+    public Shader shader2;
+    bool transitionStartingBool;
+    bool transitionEndingBool;
+    bool waiting = true;
+    public float fixedWaitingTime;
+    float elapsedWaitingTime;
+    float transStartTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (waiting)
+        {
+            elapsedWaitingTime += Time.deltaTime;
+            if (elapsedWaitingTime > fixedWaitingTime)
+            {
+                waiting = false;
+                elapsedWaitingTime = 0;
+                transitionStartingBool = true;
+            }
+        }
+        else if (transitionStartingBool)
+        {
+            freq = Mathf.Pow(transStartTime, 4.0f);
+            rend.material.SetFloat("_Frequency", freq);
+            transStartTime += Time.deltaTime;
+            if (freq > 100)
+            {
+                transitionStartingBool = false;
+                transitionEndingBool = true;
+                SwitchShader();
+            }
+        }
+        else if (transitionEndingBool)
+        {
+            freq = Mathf.Pow(transStartTime, 4.0f);
+            rend.material.SetFloat("_Frequency", freq);
+            transStartTime -= Time.deltaTime;
+            if (freq < 1)
+            {
+                transitionEndingBool = false;
+                waiting = true;
+            }
+        }
+    }
+
+    void SwitchShader()
+    {
+        if (rend.material.shader == shader1)
+        {
+            rend.material.shader = shader2;
+        }
+        else
+        {
+            rend.material.shader = shader1;
+        }
+    }
+}
---
+++ VFX Final/Assets/ShadersAndMaterials/CheckerShader/CheckerShaderAlt.shader
@@ -0,0 +1,75 @@
+﻿Shader "Unlit/CheckerShaderAlt"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_SecondTex("Texture", 2D) = "white" {}
+		_Frequency("Frequency", Range(0.1,100)) = 2.0
+	}
+		SubShader
+		{
+			Tags{ "RenderType" = "Opaque" }
+			LOD 100
+			Pass{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				// make fog work
+				#pragma multi_compile_fog
+
+				#include "UnityCG.cginc"
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				 struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					UNITY_FOG_COORDS(1)
+					float4 vertex : SV_POSITION;
+					float4 screenPos : TEXCOORD1;
+				};
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+				sampler2D _SecondTex;
+				float4 _SecondTex_ST;
+				float _Frequency;
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					//o.screenPos = ComputeScreenPos(o.vertex);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					int row = (int)(i.uv.y * _Frequency);
+					int column = (int)(i.uv.x * _Frequency);
+					fixed4 col1 = tex2D(_MainTex, i.uv);
+					fixed4 col2 = tex2D(_SecondTex, i.uv);
+					if (row % 2 == 0)
+					{
+						if (column % 2 == 0)
+						{
+							return col1;
+						}
+
+						return col2;
+					}
+					if (column % 2 == 0)
+					{
+						return col2;
+					}
+					return col1;
+					}
+				ENDCG
+			}
+		}
+}
